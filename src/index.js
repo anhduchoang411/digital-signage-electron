060d9a0b7bb3e6f35666aff1ff9e75197b09813e
Anh Duc <anhduc.hoang@vmodev.com>
2019-05-27 13:48:21 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const { app, BrowserWindow } = require('electron');
 const isDev = require('electron-is-dev');
 const url = require('url');
+const path = require('path');
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
